bb42fbd40aaae9c2bad9629272d9313945a004da
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-08-24 13:01:46 +0300

    update for APP

--- dist/src/routes/user/index.js
+++ dist/src/routes/user/index.js
@@ -5,10 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const index_1 = __importDefault(require("./auth/index"));
-const VisionMission_1 = __importDefault(require("./VisionMission"));
 const notification_1 = __importDefault(require("./notification"));
 const route = (0, express_1.Router)();
 route.use("/auth", index_1.default);
-route.use("/vision-mission", VisionMission_1.default);
 route.use("/notification", notification_1.default);
 exports.default = route;
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import AuthRoute from "./auth/index";
-import VisionMissionRouter from './VisionMission';
 import NotificationRouter from './notification';
 import { app } from "firebase-admin";
 
 const route = Router();
 route.use("/auth", AuthRoute);
-route.use("/vision-mission", VisionMissionRouter);
 route.use("/notification", NotificationRouter);
 
 export default route;
