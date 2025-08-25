6c814c85a072f4a18d0c1dc99d5ecd937ebf5f39
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-08-25 09:26:54 +0300

    update gmail

--- src/utils/sendEmails.ts
+++ src/utils/sendEmails.ts
@@ -14,7 +14,7 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
 
   try {
     const info = await transporter.sendMail({
-      from: `"My App" <${process.env.EMAIL_USER}>`,
+      from: `"Smart_collge" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
