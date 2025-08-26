1117e2fcae842a22bb71f648798f8d601c706961
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-08-26 15:32:34 +0300

    Update User.js

--- dist/src/models/shema/auth/User.js
+++ dist/src/models/shema/auth/User.js
@@ -10,7 +10,6 @@ const UserSchema = new mongoose_1.Schema({
     name: { type: String, },
     email: { type: String, unique: true },
     password: { type: String },
-    dateOfBirth: { type: Date, },
     fcmtoken: { type: String },
     isVerified: { type: Boolean, default: false },
     googleId: { type: String, unique: true, sparse: true },
