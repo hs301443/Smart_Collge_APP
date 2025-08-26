9f45fac1c8c4c25410ef4e44171220247af6fb77
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-08-26 13:57:39 +0300

    test mail again

--- dist/src/utils/sendEmails.js
+++ dist/src/utils/sendEmails.js
@@ -1,38 +1,15 @@
 "use strict";
-// import nodemailer from "nodemailer";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendEmail = void 0;
-// export const sendEmail = async (to: string, subject: string, text: string) => {
-//   console.log("Email user:", process.env.EMAIL_USER);
-//   console.log("Email pass:", process.env.EMAIL_PASS ? "Exists" : "Missing");
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"Smart_collge" <${process.env.EMAIL_USER}>`,
-//       to,
-//       subject,
-//       text,
-//     });
-//     console.log("✅ Email sent:", info.response);
-//   } catch (error) {
-//     console.error("❌ Email error:", error);
-//   }
-// };
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const sendEmail = async (to, subject, text) => {
+    console.log("Email user:", process.env.EMAIL_USER);
+    console.log("Email pass:", process.env.EMAIL_PASS ? "Exists" : "Missing");
     const transporter = nodemailer_1.default.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: Number(process.env.EMAIL_PORT),
-        secure: false, // مع 587 لازم false
+        service: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
@@ -40,7 +17,7 @@ const sendEmail = async (to, subject, text) => {
     });
     try {
         const info = await transporter.sendMail({
-            from: `"Smart College" <${process.env.EMAIL_USER}>`,
+            from: `"Smart_collge" <${process.env.EMAIL_USER}>`,
             to,
             subject,
             text,
@@ -52,3 +29,26 @@ const sendEmail = async (to, subject, text) => {
     }
 };
 exports.sendEmail = sendEmail;
+// import nodemailer from "nodemailer";
+// export const sendEmail = async (to: string, subject: string, text: string) => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: Number(process.env.EMAIL_PORT),
+//     secure: false, // مع 587 لازم false
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `"Smart College" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       text,
+//     });
+//     console.log("✅ Email sent:", info.response);
+//   } catch (error) {
+//     console.error("❌ Email error:", error);
+//   }
+// }
--- src/utils/sendEmails.ts
+++ src/utils/sendEmails.ts
@@ -1,38 +1,11 @@
-// import nodemailer from "nodemailer";
-
-// export const sendEmail = async (to: string, subject: string, text: string) => {
-//   console.log("Email user:", process.env.EMAIL_USER);
-//   console.log("Email pass:", process.env.EMAIL_PASS ? "Exists" : "Missing");
-
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"Smart_collge" <${process.env.EMAIL_USER}>`,
-//       to,
-//       subject,
-//       text,
-//     });
-//     console.log("✅ Email sent:", info.response);
-//   } catch (error) {
-//     console.error("❌ Email error:", error);
-//   }
-// };
-
-
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
+  console.log("Email user:", process.env.EMAIL_USER);
+  console.log("Email pass:", process.env.EMAIL_PASS ? "Exists" : "Missing");
+
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false, // مع 587 لازم false
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -41,7 +14,7 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
 
   try {
     const info = await transporter.sendMail({
-      from: `"Smart College" <${process.env.EMAIL_USER}>`,
+      from: `"Smart_collge" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
@@ -50,4 +23,31 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
   } catch (error) {
     console.error("❌ Email error:", error);
   }
-}
+};
+
+
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async (to: string, subject: string, text: string) => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: Number(process.env.EMAIL_PORT),
+//     secure: false, // مع 587 لازم false
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `"Smart College" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       text,
+//     });
+//     console.log("✅ Email sent:", info.response);
+//   } catch (error) {
+//     console.error("❌ Email error:", error);
+//   }
+// }
