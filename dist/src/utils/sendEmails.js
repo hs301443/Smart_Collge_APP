e070476580ed11a1cb3022f179147a3ef8504979
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-08-26 11:23:19 +0300

    update mail

--- dist/src/utils/sendEmails.js
+++ dist/src/utils/sendEmails.js
@@ -1,42 +1,46 @@
 "use strict";
-// import nodemailer from "nodemailer";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendEmail = sendEmail;
-// export const sendEmail = async (to: string, subject: string, text: string) => {
-//   console.log("Email user:", process.env.EMAIL_USER);
-//   console.log("Email pass:", process.env.EMAIL_PASS ? "Exists" : "Missing");
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"Smart_collge" <${process.env.EMAIL_USER}>`,
-//       to,
-//       subject,
-//       text,
-//     });
-//     console.log("✅ Email sent:", info.response);
-//   } catch (error) {
-//     console.error("❌ Email error:", error);
-//   }
-// };
-const resend_1 = require("resend");
-const resend = new resend_1.Resend(process.env.RESEND_API_KEY);
-async function sendEmail(to, subject, text) {
+exports.sendEmail = void 0;
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const sendEmail = async (to, subject, text) => {
+    console.log("Email user:", process.env.EMAIL_USER);
+    console.log("Email pass:", process.env.EMAIL_PASS ? "Exists" : "Missing");
+    const transporter = nodemailer_1.default.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
     try {
-        const data = await resend.emails.send({
-            from: "Smart College <noreply@smartcollege.com>",
+        const info = await transporter.sendMail({
+            from: `"Smart_collge" <${process.env.EMAIL_USER}>`,
             to,
             subject,
             text,
         });
-        console.log("✅ Email sent:", data);
+        console.log("✅ Email sent:", info.response);
     }
     catch (error) {
         console.error("❌ Email error:", error);
     }
-}
+};
+exports.sendEmail = sendEmail;
+// import { Resend } from "resend";
+// const resend = new Resend(process.env.RESEND_API_KEY!);
+// export async function sendEmail(to: string, subject: string, text: string) {
+//   try {
+//     const data = await resend.emails.send({
+//       from: "Smart College <noreply@smartcollege.com>", 
+//       to,
+//       subject,
+//       text,
+//     });
+//     console.log("✅ Email sent:", data);
+//   } catch (error) {
+//     console.error("❌ Email error:", error);
+//   }
+// }
