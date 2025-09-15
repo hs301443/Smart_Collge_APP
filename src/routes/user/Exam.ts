7cc33ed9918c29c3c52ee8e77487e1a01547b838
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-09-15 14:45:10 +0300

    update exam for users

--- src/routes/user/Exam.ts
+++ src/routes/user/Exam.ts
@@ -1,14 +1,12 @@
 import { catchAsync } from "../../utils/catchAsync";
 import { authenticated } from "../../middlewares/authenticated";
 import { Router } from "express";
-import { getExamByIdForStudent,getExamsForStudent,getMyAttempts,submitExamAttempt,startExamAttempt  } from "../../controller/users/Exam";
+import { getExamByIdForStudent,getExamsForStudent  } from "../../controller/users/Exam";
 
 const router = Router();
 
-router.get("/exams", authenticated, catchAsync(getExamsForStudent));
-router.get("/exam/:id", authenticated, catchAsync(getExamByIdForStudent));
-router.get("/attempts", authenticated, catchAsync(getMyAttempts));
-router.post("/attempt/:id/start", authenticated, catchAsync(startExamAttempt));
-router.post("/attempt/:id/submit", authenticated, catchAsync(submitExamAttempt));
+router.get("/", authenticated, catchAsync(getExamsForStudent));
+router.get("/:id", authenticated, catchAsync(getExamByIdForStudent));
+
 
 export default router;
