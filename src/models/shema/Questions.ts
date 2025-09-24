d514b65715e96cd56b1afa3a565d53c0a1947839
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-09-24 14:58:49 +0300

    exam

--- src/models/shema/Questions.ts
+++ src/models/shema/Questions.ts
@@ -9,7 +9,7 @@ const QuestionSchema = new Schema({
   exam: { type: Schema.Types.ObjectId, ref: 'Exam' },
   type: { 
     type: String, 
-    enum: ['choice','file-upload',] 
+    enum: ['choice','file-upload','short-answer'], 
   },
   text: String,
   choices: [ChoiceSchema],       // للـ MCQ بس
