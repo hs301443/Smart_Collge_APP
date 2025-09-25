f6f64f799cc6bf2fadf05621d3da5a6a18bd8f9a
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-09-25 14:01:01 +0300

    chat for admin

--- src/routes/admin/chat.ts
+++ src/routes/admin/chat.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { catchAsync } from "../../utils/catchAsync";
-import { authenticated } from "../../middlewares/authenticated";
 import { sendMessageByAdmin,getAdminChats,getMessagesByChatId } from "../../controller/admin/chat";
 
 const router = Router();
 
 
-router.post('/', catchAsync(sendMessageByAdmin))
-router.get('/messages', catchAsync(getMessagesByChatId))
+router.post('/:chatId', catchAsync(sendMessageByAdmin))
+router.get('/messages/:chatId', catchAsync(getMessagesByChatId))
 router.get('/', catchAsync(getAdminChats))
 
 export default router;
