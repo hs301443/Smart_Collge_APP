deb9c94cf78a274a68675de4d7541f5af8e5e5ce
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-09-25 11:59:42 +0300

    update

--- dist/src/routes/user/Statics.js
+++ dist/src/routes/user/Statics.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const Statics_1 = require("../../controller/users/Statics");
 const router = (0, express_1.Router)();
-router.get("/");
+router.get("/", Statics_1.graduationStats);
 exports.default = router;
--- src/routes/user/Statics.ts
+++ src/routes/user/Statics.ts
@@ -6,6 +6,7 @@ const router=Router();
 
 router.get(
   "/",
+  graduationStats
  
 );
 
